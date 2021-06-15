2d0b67a84917fab02c559466c1efa94bc6a34a59
Hyunbin Park <hyunbin.park@sensible4.fi>
2021-06-15 03:30:36 +0300

    Remove unnacessary comments and add useful scripts

--- ubuntu18.04/installation/install_basic_tools.sh
+++ ubuntu18.04/installation/install_basic_tools.sh
@@ -2,9 +2,9 @@
 
 apt-get update
 apt-get install -y \
-    lsb-release \               # for ROS-melodic
-    gnupg2 \                    # for ROS-melodic
-    dirmngr \                   # https://stackoverflow.com/questions/54380518/why-does-this-apt-key-adv-command-give-an-error
+    lsb-release \
+    gnupg2 \
+    dirmngr \
     sudo \
     cmake \
     curl \
