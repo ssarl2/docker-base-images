72ea2fc6c405d153fbc4dd14d6bdf617defe228b
Hyunbin Park <hyunbin.park1026@gmail.com>
2021-08-15 18:24:39 +0300

    Change the image destination

---
+++ ubuntu18.04_ros_melodic/READMD.md
@@ -0,0 +1,15 @@
+## How to use this
+### build
+`bash build.sh`
+### setting
+Connect your code folder to workspace folder [here]()
+### run
+`bash run.sh`
+### use
+- multiple terminals in the container [More commands of tmux](https://binibini.notion.site/how-to-use-tmux-98d24ee29b7a4612a37596f3f87807ed)
+```
+tmux
+ctrl+b, c
+ctrl+b, o
+```
+- vscode `code .`
--- ubuntu18.04_ros_melodic/utils/docker_run.sh
+++ ubuntu18.04_ros_melodic/utils/docker_run.sh
@@ -1,34 +1,9 @@
 #!/bin/bash
 
-# docker run -it --rm --name test_ub18 \
-#     --privileged \
-#     -e DISPLAY=$DISPLAY \
-#     -v $(pwd)/../workspace:/home/${USER} \
-#     -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
-#     -w /home/${USER} \
-#     robuntu:test
-
-    # -p 5900:5900 \
-# sudo docker run --name=test_ub18 -it --rm \
-#     --tmpfs /run --tmpfs /run/lock --tmpfs /tmp \
-#     --cap-add SYS_BOOT --cap-add SYS_ADMIN \
-#     -v /sys/fs/cgroup:/sys/fs/cgroup \
-#     -p 5900:5900 -p 5901:5901 -p 6901:6901 \
-#     robuntu:test
-
-# sudo apt update && sudo apt install -y x11vnc
-
-# sudo docker run --name=test_ub18 -it --rm --privileged \
-#     -e "ROS_HOSTNAME=localhost" \
-#     -e "ROS_MASTER_URI=http://localhost:11311" \
-#     --network host \
-#     -e QT_X11_NO_MITSHM=1 \
-#     -e DISPLAY=$DISPLAY \
-#     -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
-#     robuntu:test
-
 VOLUME_PATH=$PWD/../workspace
 USER_DOCKER=melodic
+IMAGE_VERSION=ssarl2/docker-base-images:v1.1
+
 xhost +
 echo "Running docker env"
 
@@ -43,5 +18,5 @@ sudo docker run --name=ub18_melodic --rm --privileged \
     -v $XAUTH:$XAUTH \
     -v $VOLUME_PATH:/home/$USER_DOCKER/test_folder \
     --runtime=nvidia \
-    ub18_melodic:latest \
+    $IMAGE_VERSION \
     tail -f /dev/null
